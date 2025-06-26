fd49bc3f04f2f5ad8a71c63d23f27b92ce1bec62
amal <amalreji158@gmail.com>
2025-06-26 16:06:45 +0530

    commit after updating buttons and adding dummy search

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1335958611"
+    serviceWorkerVersion: "486474749"
   }
 });
