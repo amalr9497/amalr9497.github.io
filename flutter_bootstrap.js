55c8f0b352347cb50ef083babb7ab9230cd1b5d6
amal <amalreji158@gmail.com>
2025-07-01 15:45:02 +0530

    updated store launch button placements

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "850649421"
+    serviceWorkerVersion: "6180435"
   }
 });
